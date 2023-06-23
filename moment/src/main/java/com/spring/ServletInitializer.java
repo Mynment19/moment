cde04c335d2879bc0a426316ada07a344a838cb7
pjh <mynment19@gmail.com>
2023-06-23 17:34:10 +0900

    moment 수정

--- moment/src/main/java/com/spring/ServletInitializer.java
+++ moment/src/main/java/com/spring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BootMember1Application.class);
+		return application.sources(MomentApplication.class);
 	}
 
 }
